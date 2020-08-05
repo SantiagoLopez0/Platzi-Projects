5fffcf3f5f7cfc2293f5e006dfff8e45377e5b59
Santiago López <asantiago0034@gmail.com>
2020-08-05 16:25:47 -0500

    Modulo Final - arreglos al proyecto del curso

--- JS-Profesional/assets/index.ts
+++ JS-Profesional/assets/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from './MediaPlayer'
 import AutoPlay from './plugins/AutoPlay'
 import AutoPause from './plugins/AutoPause'
+import AdsPlugin from './plugins/Ads/index'
 
 const video: HTMLMediaElement = document.querySelector('video')
 const playButton: HTMLElement = document.querySelector('#buttonTogglePlay')
@@ -8,15 +9,15 @@ const MuteButton: HTMLElement = document.querySelector('#buttonToggleMute')
 
 const player = new MediaPlayer(
     video,
-    [new AutoPlay(), new AutoPause()
-],)
+    [new AutoPlay(), new AutoPause(), new AdsPlugin()]
+)
 
 video.controls = true;
 playButton.onclick = () => player.togglePlay()
 MuteButton.onclick = () => player.toggleMute()
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').catch(error => {
-        console.log(error.message);
-    })
-}
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/sw.js').catch(error => {
+//         console.log(error.message);
+//     })
+// }
